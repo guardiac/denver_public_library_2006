7cbc4e5c68015048acaf3e543e84ea28476473b4
Angela Guardia <angelaguardia20@gmail.com>
2020-07-22 12:08:36 -0700

    Add and test write method for Author

--- lib/author.rb
+++ lib/author.rb
@@ -3,7 +3,14 @@ class Author
               :books
 
   def initialize(info)
+    @first_name = info[:first_name]
+    @last_name = info[:last_name]
     @name = "#{info[:first_name]} #{info[:last_name]}"
     @books = []
+  end
+
+  def write(title, date)
+    @books << Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: date})
+    @books.last
   end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -16,4 +16,17 @@ class AuthorTest < Minitest::Test
     assert_equal "Charlotte Bronte", @charlotte_bronte.name
     assert_equal [], @charlotte_bronte.books
   end
+
+  def test_it_can_write
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
+  end
+
+  def test_it_stores_books_written
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = @charlotte_bronte.write("Villette", "1853")
+
+    assert_equal [jane_eyre, villette], @charlotte_bronte.books
+  end
 end
