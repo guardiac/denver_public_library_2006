21e86fa66fced690d299a243bb5d3621403de15d
Angela Guardia <angelaguardia20@gmail.com>
2020-07-22 11:47:34 -0700

    Add Book attributes for title author and publication year

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
 class Book
-  attr_reader :title
+  attr_reader :title,
+              :author,
+              :publication_year
   def initialize(info)
     @title = info[:title]
+    @author = "#{info[:author_first_name]} #{info[:author_last_name]}"
+    @publication_year = info[:publication_date].slice(-4, 4)
   end
 end
