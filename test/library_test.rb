e0c6728b1615b38579c37a6523ab76c4fedef8c1
Angela Guardia <angelaguardia20@gmail.com>
2020-07-22 12:51:10 -0700

    Refactor setup method in LibraryTest

--- test/library_test.rb
+++ test/library_test.rb
@@ -7,6 +7,13 @@ require './lib/library'
 class LibraryTest < Minitest::Test
   def setup
     @dpl = Library.new("Denver Public Library")
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @professor = @charlotte_bronte.write("The Professor", "1857")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   def test_it_exists
@@ -20,47 +27,27 @@ class LibraryTest < Minitest::Test
   end
 
   def test_it_can_add_authors
-    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
 
-    @dpl.add_author(charlotte_bronte)
-    @dpl.add_author(harper_lee)
-
-    assert_equal [charlotte_bronte, harper_lee], @dpl.authors
+    assert_equal [@charlotte_bronte, @harper_lee], @dpl.authors
   end
 
   def test_it_can_add_books_from_authors
-    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-    professor = charlotte_bronte.write("The Professor", "1857")
-    villette = charlotte_bronte.write("Villette", "1853")
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
 
-    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-
-    @dpl.add_author(charlotte_bronte)
-    @dpl.add_author(harper_lee)
-
-    assert_equal [jane_eyre, professor, villette, mockingbird], @dpl.books
+    assert_equal [@jane_eyre, @professor, @villette, @mockingbird], @dpl.books
   end
 
   def test_publication_time_frame
-    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-    professor = charlotte_bronte.write("The Professor", "1857")
-    villette = charlotte_bronte.write("Villette", "1853")
-
-    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-
-    @dpl.add_author(charlotte_bronte)
-    @dpl.add_author(harper_lee)
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
 
     expected_cb = {:start=>"1847", :end=>"1857"}
     expected_hl = {:start=>"1960", :end=>"1960"}
 
-
-    assert_equal expected_cb, @dpl.publication_time_frame_for(charlotte_bronte)
-    assert_equal expected_hl, @dpl.publication_time_frame_for(harper_lee)
+    assert_equal expected_cb, @dpl.publication_time_frame_for(@charlotte_bronte)
+    assert_equal expected_hl, @dpl.publication_time_frame_for(@harper_lee)
   end
 end
